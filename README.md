8179755520c7412377c8872009d925a74138e80d
Subhendu Sethi <cs13b1027@iith.ac.in>
2019-11-30 12:32:13 +0530

    Added Day3 solution.

--- README.md
+++ README.md
@@ -23,3 +23,26 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division?
 
 Solution: [Day 2](https://github.com/subhendusethi/daily-coding-problem/blob/master/day_2/solution_day_2.py)
+
+## Daily Coding Problem: Problem #3 [Medium]
+
+This problem was asked by Google.
+
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+```python
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+```
+The following test should pass:
+
+```python
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+```
+Solution: [Day 3](https://github.com/subhendusethi/daily-coding-problem/blob/master/day_3/solution_day_3.py)
+
---
+++ day_3/solution_day_3.py
@@ -0,0 +1,54 @@
+import re
+
+class Node(object):
+	def __init__(self, val: str, left: 'Node'= None, right: 'Node'=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+node_separator = "$"
+value_level_separator = "_"
+
+def serialize(node: 'Node') -> str:
+	return serialize_helper(node,1)
+
+'''
+	While serializing store the level of each node along 
+	with the node value in a in order traversal.
+	
+	A node with index N will have its children as [ 2*N ] and [ (2*N) + 1 ]
+
+	e.g: for Node('root', Node('left', Node('left.left')), Node('right'))
+	we get: $left.left_4$left_2$root_1$right_3
+
+'''
+def serialize_helper(node: 'Node', level: int) -> str:
+	if node == None:
+		return ""
+	if node.left == None and node.right == None:
+		return node_separator + node.val + "_" + str(level)
+	return serialize_helper(node.left, 2*level) + node_separator + node.val + value_level_separator + str(level) + serialize_helper(node.right, 2*level + 1)
+
+def deserialize(s: str) -> 'Node':
+	if len(s) == 0:
+		return None
+	s_list = s.split(node_separator)
+	level_node_mapping = {}
+	for x in s_list:
+		if len(x) == 0:
+			continue
+		# item : [node_value , node_level]
+		item = x.split(value_level_separator)
+		level_node_mapping[int(item[1])] = Node(item[0])
+	for x in level_node_mapping.keys():
+		if 2*x in level_node_mapping:
+			level_node_mapping[x].left = level_node_mapping[2*x]
+		if 2*x + 1 in level_node_mapping:
+			level_node_mapping[x].right = level_node_mapping[2*x + 1]
+	return level_node_mapping[1]
+
+node = Node('root', Node('left', Node('left.left', Node('left.left.left'), Node('left.left.right'))), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+assert deserialize(serialize(node)).left.left.right.val == 'left.left.right'
+assert deserialize(serialize(node)).left.left.left.val == 'left.left.left'
+assert deserialize(serialize(node.left.left.right)).val == 'left.left.right'
